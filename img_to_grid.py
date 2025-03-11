7cb771c8140ef814485f2d77cf027b59884becc1
Dylan Britain <dylanlavon@gmail.com>
2025-03-10 20:11:14 -0500

    Add fivesplit to img_to_grid, add new colors, push connecting_ridge_slope images.

--- img_to_grid.py
+++ img_to_grid.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("source_img", type=str, help="Source image name from within the source_images directory.")
 parser.add_argument("size", type=int, help="Desired width / height of the square output image.")
 parser.add_argument("--binary", type=float, help="For conversions with only barriers and free nodes. Thresholds pixel alpha to 255 or 0 based on this ratio, where values above 255 * bw become opaque and below become transparent.")
+parser.add_argument("--fivesplit", type=int, nargs="+", help="For conversions with 4 edge weights and barrier nodes. Use five values from 0-255 to determine the ranges for which between them edge weights are determined.")
 args = parser.parse_args()
 
 # Save the new downscaled image
@@ -42,6 +43,43 @@ if args.binary:
             brightness = gray_pixels[x, y]  # Get grayscale intensity (0-255)
             if brightness < 255 * args.binary:
                 pixels[x, y] = colors.BLACK
+            else:
+                pixels[x, y] = colors.WHITE
+    bw_image.save(out_image_path)
+    quit()
+
+if args.fivesplit:
+    if len(args.fivesplit) != 5:
+        print("ERR: Using fivesplit requires 5 values between 0-255.")
+        quit()
+    for val in args.fivesplit:
+        if val < 0 or val > 255:
+            print("ERR: Using fivesplit requires 5 values between 0-255.")
+            quit()
+    split_points = sorted(args.fivesplit)
+
+    # Convert to grayscale for brightness analysis
+    resized_image = resized_image.convert("L")  # Convert to grayscale
+
+    # Get pixel access
+    bw_image = Image.new("RGB", resized_image.size)  # Create a new blank image
+    pixels = bw_image.load()
+    gray_pixels = resized_image.load()
+
+    # Process each pixel
+    for y in range(resized_image.height):
+        for x in range(resized_image.width):
+            brightness = gray_pixels[x, y]  # Get grayscale intensity (0-255)
+            if brightness < split_points[0]:
+                pixels[x, y] = colors.BLACK
+            elif brightness < split_points[1]:
+                pixels[x, y] = colors.FIVESPLIT_4
+            elif brightness < split_points[2]:
+                pixels[x, y] = colors.FIVESPLIT_3
+            elif brightness < split_points[3]:
+                pixels[x, y] = colors.FIVESPLIT_2
+            elif brightness < split_points[4]:
+                pixels[x, y] = colors.FIVESPLIT_1
             else:
                 pixels[x, y] = colors.WHITE
     bw_image.save(out_image_path)
